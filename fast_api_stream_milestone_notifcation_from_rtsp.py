19a976f797556c5528ed2390436516d04bd76400
“Thai <31430293+minhthai1995@users.noreply.github.com>
2024-10-22 01:18:42 +1100

    update

--- fast_api_stream_milestone_notifcation_from_rtsp.py
+++ fast_api_stream_milestone_notifcation_from_rtsp.py
@@ -19,7 +19,6 @@ from sqlalchemy import (
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
-import datetime
 
 import os
 import logging
